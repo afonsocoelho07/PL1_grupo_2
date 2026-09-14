87b67d8a6632aa2f9190e04d83a6c17bf12f39f2
bibi <biasoaresantunes@gmail.com>
2026-09-14 19:51:29 +0100

    inscrição feita

--- Lab1_evento/src/Gerir_evento/Inscricao.java
+++ Lab1_evento/src/Gerir_evento/Inscricao.java
@@ -7,5 +7,38 @@ package Gerir_evento;
  * 
  */
 public class Inscricao {
+    public enum EstadoInscricao {
+        PENDENTE, CONFIRMADA, CANCELADA
+    }
 
+    private Participante participante;
+    private Evento evento;
+    private EstadoInscricao estado;
+
+    public Inscricao(Participante participante, Evento evento) {
+        this.participante = participante;
+        this.evento = evento;
+        this.estado = EstadoInscricao.PENDENTE; // Estado inicial padrão ou seja sempre pendente 
+    }
+
+    public Participante getParticipante() {
+        return participante;
+    }
+
+    public Evento getEvento() {
+        return evento;
+    }
+
+    public EstadoInscricao getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoInscricao estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return participante.getNome() + " -> " + evento.getTitulo() + " | " + estado + " |";
+    }
 }
