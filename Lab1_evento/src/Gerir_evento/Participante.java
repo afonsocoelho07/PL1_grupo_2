9d620f79f3f1056b11a80aad6dda8292759f1729
FONSI <FONSI@SANTOS>
2026-09-14 19:48:00 +0100

    classe participante feita

--- Lab1_evento/src/Gerir_evento/Participante.java
+++ Lab1_evento/src/Gerir_evento/Participante.java
@@ -7,5 +7,25 @@ package Gerir_evento;
  * 
  */
 public class Participante {
+	
+	 private String nome;
+	    private String email;
 
-}
+	    public Participante(String nome, String email) {
+	        this.nome = nome;
+	        this.email = email;
+	    }
+
+	    public String getNome() {
+	        return nome;
+	    }
+
+	    public String getEmail() {
+	        return email;
+	    }
+
+	    @Override
+	    public String toString() {
+	        return "| Participante " + nome + " | " + email + " |";
+	    }
+	}
